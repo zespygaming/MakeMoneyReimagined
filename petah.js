1e568f0a44017f8c2691c99c344defa325c03e97
MedicGaming <not-hisalt712@hotmail.com>
2024-10-14 10:21:59 -0400

    please

--- petah.js
+++ petah.js
@@ -1817,18 +1817,18 @@
 			}
 			function naughtyWordToggle(){
 				if(document.getElementById('swearToggle').innerHTML=='Censor'){
-					document.getElementById('swearInstance1').innerHTML=' s';
+					document.getElementById('swearInstance1').innerHTML='u s';
 					document.getElementById('swearInstance2').innerHTML='anything';
 					document.getElementById('swearToggle').innerHTML='Hide';
 					document.getElementById('swearToggle').style.backgroundColor='black';
 					document.getElementById('swearToggle').style.color='white';
 				}else if(document.getElementById('swearToggle').innerHTML=='Hide'){
-					document.getElementById('swearInstance1').innerHTML='fucking s';
+					document.getElementById('swearInstance1').innerHTML='u fucking s';
 					document.getElementById('swearInstance2').innerHTML='shit';
 					document.getElementById('swearToggle').innerHTML='Show';
 					document.getElementById('swearToggle').style='background-color:white;color:black;';
 				}else{
-					document.getElementById('swearInstance1').innerHTML='f***king s';
+					document.getElementById('swearInstance1').innerHTML='u f***king s';
 					document.getElementById('swearInstance2').innerHTML='s**t';
 					document.getElementById('swearToggle').innerHTML='Censor';
 					document.getElementById('swearToggle').style='background-color:rgb(127,127,127);color:black;';
