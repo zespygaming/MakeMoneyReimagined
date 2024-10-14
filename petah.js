fa36b3096ee3ab5cf3cb9e29abbf6caf0e67ff51
MedicGaming <not-hisalt712@hotmail.com>
2024-10-14 14:52:31 -0400

    a

--- petah.js
+++ petah.js
@@ -1816,20 +1816,20 @@
 				}
 			}
 			function naughtyWordToggle(){
-				//if(document.getElementById('swearToggle').innerHTML=='Censor'){
+				if(document.getElementById('swearToggle').innerHTML=='Censor'){
 					document.getElementById('swearInstance1').innerHTML='s';
 					/*document.getElementById('swearToggle').innerHTML='Hide';
 					document.getElementById('swearToggle').style.backgroundColor='black';
-					document.getElementById('swearToggle').style.color='white';
+					document.getElementById('swearToggle').style.color='white';*/
 				}else if(document.getElementById('swearToggle').innerHTML=='Hide'){
 					document.getElementById('swearInstance1').innerHTML='fucking s';
-					document.getElementById('swearToggle').innerHTML='Show';
-					document.getElementById('swearToggle').style='background-color:white;color:black;';
+					/*document.getElementById('swearToggle').innerHTML='Show';
+					document.getElementById('swearToggle').style='background-color:white;color:black;';*/
 				}else{
 					document.getElementById('swearInstance1').innerHTML='f**king s';
-					document.getElementById('swearToggle').innerHTML='Censor';
-					document.getElementById('swearToggle').style='background-color:rgb(127,127,127);color:black;';
-				}*/
+					/*document.getElementById('swearToggle').innerHTML='Censor';
+					document.getElementById('swearToggle').style='background-color:rgb(127,127,127);color:black;';*/
+				}
 			}
 			function wrokEternal(){
 				if(document.getElementById('theOnlyThingTheyFearIsMe').innerHTML=='No'){
