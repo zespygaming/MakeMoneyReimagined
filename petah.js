8fc1482544c08056bdb095b453165ca63a36e7b4
MedicGaming <not-hisalt712@hotmail.com>
2024-10-15 06:49:53 -0400

    slowly

--- petah.js
+++ petah.js
@@ -1818,8 +1818,8 @@
 			function naughtyWordToggle(){
 				if(document.getElementById('swearToggle').innerHTML=='Censor'){
 					document.getElementById('swearInstance1').innerHTML='s';
-					/*document.getElementById('swearToggle').innerHTML='Hide';
-					document.getElementById('swearToggle').style.backgroundColor='black';
+					document.getElementById('swearToggle').innerHTML='Hide';
+					/*document.getElementById('swearToggle').style.backgroundColor='black';
 					document.getElementById('swearToggle').style.color='white';*/
 				}else if(document.getElementById('swearToggle').innerHTML=='Hide'){
 					document.getElementById('swearInstance1').innerHTML='fucking s';
